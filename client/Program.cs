2b567d3f7c9f8027bb6505359fd07d70cc845661
coolvadimka <145355993+coolvadimka@users.noreply.github.com>
2024-04-10 21:08:51 +0300

    доработал подключение ко 2 серверу

--- client/Program.cs
+++ client/Program.cs
@@ -1,17 +1,41 @@
 ﻿using System.Net.Sockets;
-using TcpClient tcpClient = new TcpClient();
+TcpClient tcpClient = new TcpClient();
 const string IP = "127.0.0.1";
-const int port = 7000;
+const int port1 = 7000;
+const int port2 = 8000;
 Console.WriteLine("Введите своё имя: ");
 string word = Console.ReadLine();
-await tcpClient.ConnectAsync(IP, port);
-Console.WriteLine($"Подключение к сверверу {IP}:{port} установлено.");
+await tcpClient.ConnectAsync(IP, port1);
+Console.WriteLine($"Подключение к сверверу {IP}:{port1} установлено.");
 var stream = tcpClient.GetStream();
-using var streamReader = new StreamReader(stream);
-using var streamWriter = new StreamWriter(stream);
+var streamReader = new StreamReader(stream);
+var streamWriter = new StreamWriter(stream);
 await streamWriter.WriteLineAsync(word);
 await streamWriter.FlushAsync();
 var otvet = await streamReader.ReadLineAsync();
+Console.WriteLine($"{otvet}");
+while (true)
+{
+    string temp = Console.ReadLine();
+    if (temp == "stop")
+    {
+        break;
+    }
+}
+await streamWriter.WriteLineAsync("END");
+await streamWriter.FlushAsync();
+//tcpClient.Close();
+
+
+tcpClient = new TcpClient();
+await tcpClient.ConnectAsync(IP, port2);
+Console.WriteLine($"Подключение к сверверу {IP}:{port2} установлено.");
+stream = tcpClient.GetStream();
+streamReader = new StreamReader(stream);
+streamWriter = new StreamWriter(stream);
+await streamWriter.WriteLineAsync(word);
+await streamWriter.FlushAsync();
+otvet = await streamReader.ReadLineAsync();
 Console.WriteLine($"{otvet}");
 while (true)
 {
