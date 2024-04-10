99d15e24259ee331383894c82f00130263b51e29
coolvadimka <145355993+coolvadimka@users.noreply.github.com>
2024-04-10 15:04:50 +0300

    Добавьте файлы проекта.

---
+++ client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using TcpClient tcpClient = new TcpClient();
+const string IP = "127.0.0.1";
+const int port = 7000;
+Console.WriteLine("Введите своё имя: ");
+string word = Console.ReadLine();
+await tcpClient.ConnectAsync(IP, port);
+Console.WriteLine($"Подключение к сверверу {IP}:{port} установлено.");
+var stream = tcpClient.GetStream();
+using var streamReader = new StreamReader(stream);
+using var streamWriter = new StreamWriter(stream);
+await streamWriter.WriteLineAsync(word);
+await streamWriter.FlushAsync();
+var otvet = await streamReader.ReadLineAsync();
+Console.WriteLine($"{otvet}");
+while (true)
+{
+    string temp = Console.ReadLine();
+    if (temp == "stop")
+    {
+        break;
+    }
+}
+await streamWriter.WriteLineAsync("END");
+await streamWriter.FlushAsync();
---
+++ testslaba2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+const string IP = "127.0.0.1";
+const int port = 7000;
+var tcpListener = new TcpListener(IPAddress.Any, 7000);
+try
+{
+    tcpListener.Start();
+    Console.WriteLine($"Сервер {IP}:{port} запущен. Ожидание подключений... ");
+
+    while (true)
+    {
+        using var tcpClient = await tcpListener.AcceptTcpClientAsync();
+        var stream = tcpClient.GetStream();
+        using var streamReader = new StreamReader(stream);
+        using var streamWriter = new StreamWriter(stream);
+        var name = await streamReader.ReadLineAsync();
+        Console.WriteLine($"Пользователь с именем {name} подключился к серверу.");
+        var otvet = "Привет, " + name + "!!!";
+        await streamWriter.WriteLineAsync(otvet);
+        await streamWriter.FlushAsync();
+        while (true)
+        {
+            var word = await streamReader.ReadLineAsync();
+            if (word == "END")
+            {
+                Console.WriteLine($"Пользователь с именем {name} отключился от сервера.");
+                break;
+            }
+        }
+    }
+}
+finally
+{
+    tcpListener.Stop();
+}
